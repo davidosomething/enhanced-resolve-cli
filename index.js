b7f6354347d22bd79284cf438b0654a94b1273b4
David O'Trakoun <dotrakoun@squarespace.com>
2017-11-01 11:01:01 -0400

    remove package lock, add debug flag, jsdoc, resolve basepath

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
-const VERSION = require('./package.json').version;
+/**
+ * @typedef ProgramOptions
+ * @property {object}   options
+ * @property {string}   options.basepath=process.cwd()
+ * @property {boolean}  options.debug=false
+ * @property {boolean}  options.suppress=false
+ * @property {string}   [options.webpackConfig]
+ */
+
+const package = require('./package.json');
+const [ VERSION, DESCRIPTION ] = [ package.version, package.description ];
 
 const fs = require('fs');
 const path = require('path');
@@ -9,14 +19,14 @@ const program = require('commander');
 const makeResolver = require('enhanced-resolve').create;
 
 const WEBPACK_PATH = path.join('node_modules', 'webpack', 'lib', 'webpack.js');
-const WEBPACK_CONFIGFILE = 'webpack.config.js'
+const WEBPACK_CONFIG_FILENAME = 'webpack.config.js'
 
 /**
- * @return {string[]} array of paths from startdir upwards
+ * @return {string[]} array of paths from startDir upwards
  */
-const getTree = (startdir) => {
-  let current = startdir;
-  const root = path.parse(startdir).root;
+const getTree = (startDir) => {
+  let current = startDir;
+  const root = path.parse(startDir).root;
   const paths = [];
   while (current !== root) {
     paths.push(current);
@@ -28,24 +38,33 @@ const getTree = (startdir) => {
 /**
  * Find something relative to where this program is run
  */
-const findLocal = (startdir = process.cwd(), needle) => {
-  const pathTree = getTree(startdir);
+const findLocal = (startDir = process.cwd(), needle) => {
+  const pathTree = getTree(startDir);
   const result = pathTree.findIndex((p) => fs.existsSync(path.join(p, needle)));
   if (result ===  -1) { return; }
   return path.join(pathTree[result], needle);
 };
 
+/**
+ * @param {string} request file
+ * @param {ProgramOptions} options
+ */
 const formatter = (request, options) => (err, filepath) => {
   if (err) {
     if (!options.suppress) {
-      console.error(`Could not resolve ${request} from ${options.basepath}`);
+      console.info(`Could not resolve ${request} from ${options.basepath}`);
     }
     process.exit(1);
   }
   console.log(filepath);
 };
 
-const getWebpackOptions = (webpackConfigPath, webpackPath) => {
+/**
+ * @param {string} webpackConfigFile full path to a webpack.config.js
+ * @param {string} webpackModule full path to node_modules/webpack
+ * @return {?object} webpack options object
+ */
+const getWebpackOptions = (webpackConfigFile, webpackModule) => {
   let webpack;
   let webpackCompiler;
 
@@ -56,7 +75,7 @@ const getWebpackOptions = (webpackConfigPath, webpackPath) => {
       ? webpackConfig()
       : webpackConfig;
 
-    webpack = require(require.resolve(webpackPath));
+    webpack = require(require.resolve(webpackModule));
     webpackCompiler = webpack(webpackConfig);
 
     // @TODO support options.multiConfigIndex
@@ -69,29 +88,48 @@ const getWebpackOptions = (webpackConfigPath, webpackPath) => {
   return null;
 };
 
-const getResolverOptions = (request, options = {}) => {
-  const localWebpackConfig = findLocal(options.basepath, options.webpackConfig);
-  const localWebpackPath = findLocal(options.basepath, WEBPACK_PATH);
-  const resolverOptions = localWebpackConfig && localWebpackPath
-    ? getWebpackOptions(localWebpackConfig, localWebpackPath) || {}
+/**
+ * @param {string} request file
+ * @param {ProgramOptions} options
+ * @return {object} webpack resolver options for use in enhanced-resolve
+ */
+const getResolverOptions = (request, options) => {
+  const webpackConfigFile = options.webpackConfig
+    || findLocal(options.basepath, WEBPACK_CONFIG_FILENAME);
+  if (options.debug) { console.info('Webpack config file: ', webpackConfigFile); }
+
+  const webpackModule = findLocal(options.basepath, WEBPACK_PATH);
+  if (options.debug) { console.info('Webpack module: ', webpackModule); }
+
+  const resolverOptions = webpackConfigFile && webpackModule
+    ? getWebpackOptions(webpackConfigFile, webpackModule) || {}
     : {};
 
   return resolverOptions;
 };
 
-const outputResult = (request, options = {}) => {
-  if (!request) { program.help(); }
+/**
+ * @param {string} request file
+ * @param {ProgramOptions} options
+ */
+const outputResult = (request, options) => {
+  const startDir = path.resolve(options.basepath);
+  if (options.debug) { console.info('Basepath: ', startDir); }
+
   const resolverOptions = getResolverOptions(request, options);
   const resolver = makeResolver(resolverOptions);
-  //console.log({}, options.basepath, request, formatter(request, options));
-  resolver({}, options.basepath, request, formatter(request, options));
+  resolver({}, startDir, request, formatter(request, options));
 };
 
 program
   .version(VERSION)
+  .description(DESCRIPTION)
   .arguments('<request>', 'Thing to resolve using enhanced-resolve')
-  .option('-s, --suppress', 'Suppress error output')
-  .option('-b, --basepath <path>', `Path to resolve from <${process.cwd()}>`, process.cwd())
-  .option('-w, --webpackConfig <webpackConfig>', `Path to a webpack.config.js file`, WEBPACK_CONFIGFILE)
+  .option('-b, --basepath <basepath>', `Path to resolve from <${process.cwd()}>`, process.cwd())
+  .option('-d, --debug', 'Output debugging info', false)
+  .option('-s, --suppress', 'Suppress error output', false)
+  .option('-w, --webpackConfig <webpackConfig>', `Path to a webpack.config.js file`)
   .action(outputResult)
   .parse(process.argv);
+
+if (!program.args.length) { program.help(); }
